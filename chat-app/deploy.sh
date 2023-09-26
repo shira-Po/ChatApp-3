4910340964b339f40505d58631d5620b2868c484
shira-po <99shipol@gmail.com>
2023-09-26 15:23:06 +0300

    deploy.sh update s3

--- chat-app/deploy.sh
+++ chat-app/deploy.sh
@@ -14,15 +14,9 @@ commit_hash=$2
 if docker image inspect my-chatapp:${version} > /dev/null 2>&1; then
   # The Docker image exists
   echo "The Docker image my-chatapp:${version} already exists."
-  read -p "Do you want to rebuild the image? (y/n) " rebuild
 
-  if [[ "$rebuild" == "y" ]]; then
-    # Delete the existing Docker image
-    docker rmi my-chatapp:${version}
-
-    # Build the Docker image
-    docker build -t my-chatapp:${version} .
-  fi
+  # Build the Docker image
+  docker build -t my-chatapp:${version} .
 else
   # The Docker image does not exist
   echo "The Docker image my-chatapp:${version} does not exist."
@@ -31,34 +25,26 @@ else
   docker build -t my-chatapp:${version} .
 fi
 
-# Check if the Docker build was successful
-if [ $? -ne 0 ]; then
-  echo "Docker build failed"
-  exit 1
-fi
+# Check if the commit hash exists
+if docker image inspect my-chatapp:${commit_hash} > /dev/null 2>&1; then
+  # The commit hash exists
+  echo "The commit hash my-chatapp:${commit_hash} already exists."
+  read -p "Do you want to tag and push the image to GitHub? (y/n) " tag_and_push
 
-# Push the Docker image to GitHub Container Registry
-echo "Pushing Docker image to GitHub Container Registry..."
-docker push my-chatapp:${version}
+  if [[ "$tag_and_push" == "y" ]]; then
+    # Tag the Docker image with the commit hash
+    docker tag my-chatapp:${version} my-chatapp:${commit_hash}
 
-# Tag the Docker image with the commit hash
-docker tag my-chatapp:${version} my-chatapp:${commit_hash}
+    # Push the Docker image to GitHub Container Registry
+    docker push my-chatapp:${commit_hash}
 
-
-# Check if the Docker push was successful
-if [ $? -ne 0 ]; then
-  echo "Docker push failed"
-  exit 1
-fi
-
-# Push to GitHub
-git tag ${version}
-git push origin ${version}
-
-# Check if the Git push was successful
-if [ $? -ne 0 ]; then
-  echo "Git push failed"
-  exit 1
+    # Push to GitHub
+    git tag ${version}
+    git push origin ${version}
+  fi
+else
+  # The commit hash does not exist.
+  echo "The commit hash my-chatapp:${commit_hash} does not exist."
 fi
 
 echo "Done!"
